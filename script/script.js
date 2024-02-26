7dfebfb38e0a2d9a4a30fe7e20cd3cec171c7ab6
Igor <rachik2008@gmail.com]>
2024-02-26 17:51:33 +0200

    create h/w15

--- script/script.js
+++ script/script.js
@@ -1,35 +1,74 @@
-let username = document.querySelector("#nameInput");
-let age = document.querySelector("#ageInput");
-let cookieTime = document.querySelector("#cookieTimeInput");
-let submit = document.querySelector("button");
+let arrayContacts = [];
 
-let loadName = getCookie("username");
-let loadAge = getCookie("age");
+let contactName = document.querySelector("#contactName");
+let contactNumber = document.querySelector("#contactNumber");
+let submit = document.querySelector("#submit");
+let contactsZone = document.getElementById("contactsZone");
 
-function setCookie(cName, cValue, expDays) {
-    let date = new Date();
-    date.setTime(date.getTime() + expDays * 24 * 60 * 60 * 1000);
-    let expires = "expires=" + date.toUTCString();
-    document.cookie = cName + "=" + cValue + "; " + expires + "; path=/";
+window.onload = () => {
+    arrayContacts = JSON.parse(localStorage.getItem("contacts"));
+
+    if (arrayContacts != null) {
+        arrayContacts.forEach((contact) => {
+            contactsZone.innerHTML += `
+    <tr>
+        <td>${contact.name}</td>
+        <td>${contact.number}</td>
+        <td>${contact.id}</td>
+        <td><button class="deleteButton" onclick="deletePerson(event)">X</button></td>
+    </tr>
+    `;
+        });
+    } else {
+        arrayContacts = [];
+    }
+};
+
+function Contact(name, number, id) {
+    this.name = name;
+    this.number = number;
+    this.id = id;
 }
 
-function getCookie(name) {
-    let cookieArr = document.cookie.split(";");
-    for (let i = 0; i < cookieArr.length; i++) {
-        let cookiePair = cookieArr[i].split("=");
-        if (name == cookiePair[0].trim()) {
-            return decodeURIComponent(cookiePair[1]);
+submit.addEventListener("click", function (event) {
+    event.preventDefault();
+    let usernameID = 1;
+    arrayContacts.forEach((contact) => {
+        if (contact.name === contactName.value) {
+            usernameID += 1;
+        }
+    });
+    if (contactName.value.length > 0 && contactNumber.value.length > 0) {
+        let newUser = new Contact(
+            contactName.value,
+            contactNumber.value,
+            usernameID
+        );
+        arrayContacts.push(newUser);
+        localStorage.setItem("contacts", JSON.stringify(arrayContacts));
+
+        contactName.value = "";
+        contactNumber.value = "";
+
+        console.log(arrayContacts);
+    } else {
+        alert("все поля должны быт наполнеными");
+    }
+    location.reload();
+});
+function deletePerson(event) {
+    let name =
+        event.currentTarget.parentNode.parentNode.children[0].innerText.trim();
+    let id =
+        event.currentTarget.parentNode.parentNode.children[2].innerText.trim();
+
+    for (i = 0; i < arrayContacts.length; i++) {
+        if (arrayContacts[i].name == name && arrayContacts[i].id == id) {
+            arrayContacts.splice(i, 1);
+
+            localStorage.setItem("contacts", JSON.stringify(arrayContacts));
         }
     }
 
-    return null;
-}
-
-submit.addEventListener("click", function () {
-    setCookie("username", username.value, cookieTime.value);
-    setCookie("age", age.value, cookieTime.value);
-});
-if (loadName != null && loadAge != null) {
-    document.write(loadName + "<br>");
-    document.write(loadAge);
+    location.reload();
 }
