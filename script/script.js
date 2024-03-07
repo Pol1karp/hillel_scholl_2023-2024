5eaf6279d0834d6239c1fc2071168a98da65f29a
Igor <rachik2008@gmail.com]>
2024-03-07 17:59:36 +0200

    create h/w17

--- script/script.js
+++ script/script.js
@@ -1,29 +1,33 @@
-let time = document.querySelector(".time");
-let submit = document.querySelector(".submit");
-let deleteInterval = document.querySelector(".stopInterval");
-let progressBar = document.querySelector(".progressBar");
-let timeInterval = undefined;
-let progressBarInterval = undefined;
-let websiteTime = 0;
+let agencyButtons = Array.from(document.getElementsByClassName("agencyButton"));
 
-submit.addEventListener("click", function (event) {
-    event.preventDefault();
-    timeInterval = setInterval(timeMeasure, Number(time.value * 1000 * 60));
-    progressBarInterval = setInterval(function () {
-        websiteTime += 1000;
-        progressBar.value = websiteTime / 60 / time.value / 1000;
-    }, 1000);
+let astronautsContainer = document.querySelector("#astronautsBox");
+
+var astronautsGlobal = undefined;
+
+fetch("https://api.spacexdata.com/v4/crew")
+    .then((response) => {
+        return response.json();
+    })
+    .then((astronauts) => {
+        astronautsGlobal = astronauts;
+    });
+agencyButtons.forEach((element) => {
+    element.addEventListener("click", function (event) {
+        astronautsContainer.innerHTML = "";
+        astronautsGlobal.forEach((astronaut) => {
+            if (astronaut.agency === event.target.innerText) {
+                astronautsContainer.innerHTML += `
+    <div class="card col-3 mt-5" style="width: 18rem;">
+        <img src="${astronaut.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">${astronaut.name}</h5>
+        <p class="card-text">Agency: ${astronaut.agency}<br>
+        Wikipedia: <a>${astronaut.wikipedia}</a> </p>
+        <a href="${astronaut.wikipedia}" class="btn btn-primary mt-auto">Проверь его или ее wiki</a>
+        </div>
+    </div>
+    `;
+            }
+        });
+    });
 });
-
-deleteInterval.addEventListener("click", stopInterval);
-
-function timeMeasure() {
-    websiteTime = 0;
-    alert(`Время для отдыха), Вы роботаете  ${Number(time.value)} минут`);
-}
-
-function stopInterval(event) {
-    event.preventDefault();
-    clearInterval(timeInterval);
-    clearInterval(progressBarInterval);
-}
